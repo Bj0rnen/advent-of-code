67e3b5bf42d720f84493758eb91eede2dad67146
Björn Hegerfors <bj0rn@spotify.com>
2019-12-21 18:36:24 +0100

    Day18, part 2. Slow again, 15 minutes. And very much memory used.

--- 2019/18/Main.hs
+++ 2019/18/Main.hs
@@ -9,19 +9,23 @@ import qualified Data.Set as Set
 
 import Debug.Trace
 
+findCoords :: Char -> UArray (Int, Int) Char -> [(Int, Int)]
+findCoords c m = [coord | coord <- range (bounds m), m ! coord == c]
+
 findCoord :: Char -> UArray (Int, Int) Char -> (Int, Int)
-findCoord c m = head [coord | coord <- range (bounds m), m ! coord == c]
+findCoord c = head . findCoords c
 
 countCoords :: (Char -> Bool) -> UArray (Int, Int) Char -> Int
 countCoords pred m = length [coord | coord <- range (bounds m), pred (m ! coord)]
 
 data State = State
     { numKeys :: Int
-    , pos     :: (Int, Int)
+    , pos     :: [(Int, Int)]
     , keys    :: Set Char
     } deriving (Show, Eq, Ord)
 
 data Cell = Invalid | Step | Key Char
+    deriving (Show)
 
 visit :: Set Char -> Char -> Cell
 visit ks cell
@@ -31,27 +35,52 @@ visit ks cell
         if Set.member (toLower cell) ks then Step else Invalid
     | cell >= 'a' && cell <= 'z' = Key cell
 
-newState :: (Int, Int) -> Set Char -> Cell -> Maybe State
-newState _ _ Invalid = Nothing
-newState pos keys Step = Just (State { numKeys = Set.size keys, ..})
-newState pos oldKeys (Key k) =
-    let keys = Set.insert k oldKeys
-    in  Just (State { numKeys = Set.size keys, ..})
+isInteresting :: Char -> Bool
+isInteresting cell
+    | cell == '@' = True
+    | cell >= 'a' && cell <= 'z' = True
+    | otherwise = False
+
+multipleNonInteresting :: [Char] -> Bool
+multipleNonInteresting = (> 1) . length . filter (not . isInteresting)
+
+newState :: [(Int, Int)] -> [Char] -> Set Char -> Cell -> Maybe State
+newState _ _ _ Invalid = Nothing
+newState pos cs keys Step =
+    if multipleNonInteresting cs then
+        Nothing
+    else
+        Just (State { numKeys = Set.size keys, ..})
+newState pos cs oldKeys (Key k) =
+    if multipleNonInteresting cs then
+        Nothing
+    else
+        let keys = Set.insert k oldKeys
+        in  Just (State { numKeys = Set.size keys, ..})
+
+onEachIndividually :: (a -> (a, b)) -> [a] -> [([a], b)]
+onEachIndividually f [] = []
+onEachIndividually f (x : xs) =
+    (let (x', y) = f x in (x' : xs, y))
+    :
+    (map (\(xs', y) -> (x : xs', y)) (onEachIndividually f xs))
 
 nextStates :: UArray (Int, Int) Char -> State -> Set State
-nextStates m (State n (x, y) ks) =
-    let dirs =
-            [ (x    , y - 1)
-            , (x    , y + 1)
-            , (x - 1, y    )
-            , (x + 1, y    )
-            ]
-    in  Set.fromList $ catMaybes $ map (\pos -> newState pos ks (visit ks (m ! pos))) dirs
+nextStates m (State n ps ks) =
+    let alternatives = do
+            f <-
+                [ \(x, y) -> (x    , y - 1)
+                , \(x, y) -> (x    , y + 1)
+                , \(x, y) -> (x - 1, y    )
+                , \(x, y) -> (x + 1, y    )
+                ]
+            onEachIndividually (fmap (\coord -> (coord, visit ks (m ! coord))) f) ps
+    in  Set.fromList $ catMaybes $ map (\(ps', cell) -> newState ps' (map (\p -> m ! p) ps') ks cell) alternatives
 
-solve1 :: UArray (Int, Int) Char -> Int
-solve1 m = go m 0 (Set.singleton (State 0 start Set.empty)) Set.empty
+solve :: UArray (Int, Int) Char -> Int
+solve m = go m 0 (Set.singleton (State 0 entrances Set.empty)) Set.empty
     where
-        start = findCoord '@' m
+        entrances = findCoords '@' m
         totalKeys = countCoords (\c -> c >= 'a' && c <= 'z') m
         go m steps states statesEverTried
             | (trace "Max keys found:" (traceShowId (numKeys (Set.findMax states)))) == totalKeys = steps
@@ -63,20 +92,32 @@ solve1 m = go m 0 (Set.singleton (State 0 start Set.empty)) Set.empty
                         (Set.difference (Set.foldl' (\ss s -> Set.union ss (nextStates m s)) Set.empty states) statesEverTried)
                         (Set.union statesEverTried states)
 
+patch :: UArray (Int, Int) Char -> UArray (Int, Int) Char
+patch m =
+    let (x, y) = findCoord '@' m
+    in  m // [ ((x - 1, y - 1), '@'), ((x    , y - 1), '#'), ((x + 1, y - 1), '@')
+             , ((x - 1, y    ), '#'), ((x    , y    ), '#'), ((x + 1, y    ), '#')
+             , ((x - 1, y + 1), '@'), ((x    , y + 1), '#'), ((x + 1, y + 1), '@')
+             ]
+
+parse :: String -> UArray (Int, Int) Char
+parse s =
+    let ls = lines s
+        rows = length ls
+        cols = length $ ls !! 0
+    in  listArray ((0, 0), (cols - 1, rows - 1)) (concat $ transpose ls)
 
 a :: IO Int
 a = do
-    ls <- lines <$> readFile "input.txt"
-    let rows = length ls
-        cols = length $ ls !! 0
-        m = listArray ((0, 0), (cols - 1, rows - 1)) (concat $ transpose ls)
-    return $ solve1 m
+    m <- parse <$> readFile "input.txt"
+    return $ solve m
 
 b :: IO Int
 b = do
-    undefined
+    m <- patch . parse <$> readFile "input.txt"
+    return $ solve m
 
 main :: IO ()
 main = do
-    a >>= print
-    --b >>= print
+    --a >>= print
+    b >>= print
