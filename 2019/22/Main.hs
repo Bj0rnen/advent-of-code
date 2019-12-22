95acbf5aab3abb124d70199138b709c7a1ecfc1d
Björn Hegerfors <bj0rn@spotify.com>
2019-12-22 20:01:08 +0100

    Optimize imports and extensions

--- 2019/22/Main.hs
+++ 2019/22/Main.hs
@@ -1,44 +1,21 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
-import Data.Functor
 import Data.Void
 import Data.Maybe
-import Data.Array.Unboxed
-import Data.Array.MArray
-import Data.Array.ST
-import Control.Monad
-import Control.Monad.ST
-import Control.Monad.State
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.List
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Bifunctor
 import Data.Semigroup
-import Data.FiniteField.PrimeField
+import Data.FiniteField
 import GHC.TypeNats
-import Debug.Trace
 
 type Parser = Parsec Void String
 
 justParse :: (Stream s, Ord e) => Parsec e s a -> s -> a
 justParse p s = fromJust $ parseMaybe p s
+
 
 data Shuffle p = Shuffle
     { multiplyBy :: PrimeField p
