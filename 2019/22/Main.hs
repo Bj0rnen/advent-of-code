8df46e7b91364d2b7d3447f05ebfec1532ec5458
Björn Hegerfors <bj0rn@spotify.com>
2019-12-22 19:56:08 +0100

    Cleanup

--- 2019/22/Main.hs
+++ 2019/22/Main.hs
@@ -9,6 +9,7 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 import Text.Megaparsec
@@ -39,29 +40,31 @@ type Parser = Parsec Void String
 justParse :: (Stream s, Ord e) => Parsec e s a -> s -> a
 justParse p s = fromJust $ parseMaybe p s
 
-data Technique =
-    DINS
-    | C Integer
-    | DWI Integer
+data Shuffle p = Shuffle
+    { multiplyBy :: PrimeField p
+    , thenAdd :: PrimeField p
+    } deriving (Show, Eq, Ord)
 
-parseTechnique :: Parser Technique
-parseTechnique = do
-    DINS <$ string "deal into new stack"
-    <|> C <$> (string "cut " *> L.signed (return ()) L.decimal)
-    <|> DWI <$> (string "deal with increment " *> L.decimal)
-
-readInput :: String -> IO [Technique]
-readInput file = do
-    s <- readFile file
-    return $ map (justParse parseTechnique) $ lines s
-
-data Shuffle p = Shuffle { multiplyBy :: PrimeField p, thenAdd :: PrimeField p }
-    deriving (Show, Eq, Ord)
 instance KnownNat p => Semigroup (Shuffle p) where
     (Shuffle a b) <> (Shuffle c d) =
         Shuffle (a * c) (b * c + d)
 instance KnownNat p => Monoid (Shuffle p) where
     mempty = Shuffle 1 0
+
+dealIntoNewStack    = Shuffle (-1) (-1)
+cut               n = Shuffle 1    (-n)
+dealWithIncrement n = Shuffle n    0
+
+
+parseTechnique :: KnownNat p => Parser (Shuffle p)
+parseTechnique = do
+    dealIntoNewStack <$ string "deal into new stack"
+    <|> cut . fromIntegral <$> (string "cut " *> L.signed (return ()) L.decimal)
+    <|> dealWithIncrement . fromIntegral <$> (string "deal with increment " *> L.decimal)
+
+readInput :: KnownNat p => String -> IO [Shuffle p]
+readInput file = map (justParse parseTechnique) . lines <$> readFile file
+
 
 lookupCard :: KnownNat p => PrimeField p -> Shuffle p -> PrimeField p
 lookupCard index (Shuffle {..}) = index * multiplyBy + thenAdd
@@ -69,20 +72,15 @@ lookupCard index (Shuffle {..}) = index * multiplyBy + thenAdd
 lookupIndex :: KnownNat p => PrimeField p -> Shuffle p -> PrimeField p
 lookupIndex card (Shuffle {..}) = (card - thenAdd) / multiplyBy
 
-toShuffle :: KnownNat p => Technique -> Shuffle p
-toShuffle DINS = Shuffle (-1) (-1)
-toShuffle (C n) = Shuffle 1 (fromIntegral (-n))
-toShuffle (DWI n) = Shuffle (fromIntegral n) 0
-
 
 a :: IO (PrimeField 10007)
 a = do
-    shuffles <- map toShuffle <$> readInput "input.txt"
+    shuffles <- readInput "input.txt"
     return $ lookupCard 2019 $ mconcat shuffles
 
 b :: IO (PrimeField 119315717514047)
 b = do
-    shuffles <- map toShuffle <$> readInput "input.txt"
+    shuffles <- readInput "input.txt"
     return $ lookupIndex 2020 $ stimes 101741582076661 $ mconcat shuffles
 
 main :: IO ()
